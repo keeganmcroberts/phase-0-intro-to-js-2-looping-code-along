4e10da87c7177c4e8d363a6923f6612e7676aa56
Keegan McRoberts <keegan.mcroberts@gmail.com>
2022-05-25 11:53:42 -0500

    commit tree 8cad7e3aaf62f18dfc3fc618444311f860a81891

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// Code your solutions in this filefor (let age = 30; age < 40; age++) {
+    const name = ["Guadalupe", "Ollie", "Aki"]
+    const event = ["surprise"]
+    const cards = []
+
+    function writeCards(name, event){
+        for (let i = 0; i < 3 ; i++) {
+            cards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+            console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        }
+        return cards;
+     
+    }
+
+    function countDown(n){
+        for (let i = n; i >= 0; i-- ) {
+            console.log(i)
+        }
+    }
+
